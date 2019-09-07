7f5501fde31f1df6adc09c818437b0f53f6595c4
John Eckert <johnteckert@gmail.com>
2019-09-07 14:55:45 -0400

    sends test json array

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var testData = [
+    {
+        name: "Penelope",
+        breed: "cat",
+        age: 4
+    },
+    {
+        name: "Winston",
+        breed: "cat",
+        age: 4
+    },
+    {
+        name: "Wesley",
+        breed: "chihuahua",
+        age: 8
+    },
+    {
+        name: "Lucy",
+        breed: "dachsund",
+        age: 10
+    },
+    {
+        name: "Masie",
+        breed: "Dorkie",
+        age: 6
+    }
+]
+
+app.get('/', (req, res) => res.send(JSON.stringify(testData)))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
